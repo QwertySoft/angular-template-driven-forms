e854888111f5fa40ee759c7d667599bf47595197
Máximo Zarza <zmaximo1990@gmail.com>
2018-10-18 10:25:16 -0300

    Example of pipes usage

--- src/app/exponential.pipe.ts
+++ src/app/exponential.pipe.ts
@@ -10,9 +10,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ExponentialPipe implements PipeTransform {
 
   transform(number: any, exponent: any): any {
+    // Si lo que recibimos como numero no es un numero, mostrabmos blanco
     if (isNaN(number)) {
       return '';
     }
+    // Elevamos el numero al exponente. Si este ultimo no es un numero, el valor por defecto es 1
     return Math.pow(number, isNaN(exponent) ? 1 : exponent);
   }
 

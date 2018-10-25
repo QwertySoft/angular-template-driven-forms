b64fa688a18893ea786045055f57a9b35f7ec7b8
Máximo Zarza <zmaximo1990@gmail.com>
2018-10-25 00:51:11 -0300

    Add avatares

--- server/routes/clients.js
+++ server/routes/clients.js
@@ -12,7 +12,8 @@ router.get('/clients', function (req, res) {
 		phone: '2983565091',
 		address: 'Calle 6 635 1D',
 		email: 'zmaximo1990@gmail.com',
-		instagram: 'https://www.instagram.com/zarzamaximo/'
+		instagram: 'https://www.instagram.com/zarzamaximo/',
+		avatar: 'https://media.tenor.com/images/d02f68f2b8785baa2e72115dec9bceed/tenor.gif'
 	});
 	result.push({
 		id: 2,
@@ -20,7 +21,8 @@ router.get('/clients', function (req, res) {
 		phone: null,
 		address: 'Calle 61 522 2do 6to, ADV, B1904 La Plata, Pcia de Buenos Aires',
 		email: 'qwertysoftar@gmail.com',
-		instagram: 'https://www.instagram.com/qwertysoftar/'
+		instagram: 'https://www.instagram.com/qwertysoftar/',
+		avatar: null
 	});
 	
 	res.status(200).json(result);
